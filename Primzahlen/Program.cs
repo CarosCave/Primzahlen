ddba95d88a5fe84333fbd13254165ba81aefe2d8
Caro <15948931+CarosCave@users.noreply.github.com>
2023-01-30 09:36:03 +0100

    Init

---
+++ Primzahlen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+int primZahlStart = 1;
+int primZahlEnde = 1000;
+int tempZähler = 0;
+
+int aktuellZuPrüfendeZahl = primZahlStart;
+
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+
+for (int i = 0; i < primZahlEnde; i++)
+{
+    for (int j = primZahlStart; j <= aktuellZuPrüfendeZahl; j++)
+    {
+        if ((aktuellZuPrüfendeZahl % j) == 0)
+        {
+            tempZähler++;
+        }  
+    }
+
+    if (tempZähler == 1 || tempZähler == 2)
+    {
+        Console.Write(aktuellZuPrüfendeZahl + ", ");
+    }
+    aktuellZuPrüfendeZahl++;
+    tempZähler = 0;
+}
+
+stopWatch.Stop();
+
+Console.WriteLine();
+Console.WriteLine(stopWatch.Elapsed);
